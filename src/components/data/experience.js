b0df8103a416fa12da73aca6c553f376be9a3b85
luantran <the-luan.tran@mail.mcgill.ca>
2025-12-18 23:58:14 -0500

    added education section

---
+++ src/components/data/experience.js
@@ -0,0 +1,56 @@
+export const experienceData = [
+    {
+        hash: "a3f52b9", // or auto-generate
+        title: "Web Automation Developer Intern",
+        company: "Broadsign",
+        period: "Jan - May 2019",
+        // description: "Brief overview...",
+        achievements: [
+            "Designed a new automation framework that accurately reproduce a live environment against which integration\n" +
+        "and system tests can be run",
+            "Automated the clean deployment and database population of a web server using Docker and Bitbucket Pipelines",
+            "Developed a Python REST tool library which developers could use to write their unit and integration tests"
+        ],
+        // projects: [ // optional array
+        //     { name: "Dashboard Redesign", link: "..." }
+        // ],
+        // impact: "40% increase in engagement", // optional
+        tech: ["Python", "Docker", "Bitbucket"],
+    },
+    {
+        hash: "7c2d81e", // or auto-generate
+        title: "Software Developer Intern",
+        company: "Ericsson",
+        period: "May - Dec 2018",
+        // description: "Brief overview...",
+        achievements: [
+            "Automated software installation processes to decrease runtime by 15% using Ansible",
+            "Implemented a build certification process in custom Concourse to improve testing team’s efficiency by 75%",
+            "Developed automated Oracle backup and restore solution to decrease Concourse jobs’ runtime by 20%",
+            "Proposed and implemented new dispatch process for CI infrastructure by integrating Mantis API to Concourse"
+        ],
+        // projects: [ // optional array
+        //     { name: "Dashboard Redesign", link: "..." }
+        // ],
+        // impact: "40% increase in engagement", // optional
+        tech: ["Python", "Mantis", "Concourse", "Ansible"],
+    },
+    {
+        hash: "1f9a3bc", // or auto-generate
+        title: "Automation SQA Intern",
+        company: "Matrox",
+        period: "Jan - Aug 2017",
+        // description: "Brief overview...",
+        achievements: [
+            "Created a new automated testing workflow to improve reduce weekly manual testing by 10 hours",
+            "Integrated Silktest, Jenkins and Testrail API using Python scripts to 100% automate log parsing, reporting and archiving",
+            "Set up, configured and managed Jenkins servers and slaves",
+            "Developed Python and 4Test test suites to perform functional and performance tests"
+        ],
+        // projects: [ // optional array
+        //     { name: "Dashboard Redesign", link: "..." }
+        // ],
+        // impact: "40% increase in engagement", // optional
+        tech: ["Python", "Bash", "Silktest", "Jenkins", "Testrail"],
+    }
+]
